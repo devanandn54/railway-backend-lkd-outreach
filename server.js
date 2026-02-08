eb76bd6f287a3c2677ec457b0b8c5bb4a9a42cbe
devanandn54 <devanand.1si18ei030@gmail.com>
2026-02-07 22:35:15 -0500

    fixed trust proxy error

--- server.js
+++ server.js
@@ -306,6 +306,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 // Configuration
